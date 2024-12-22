2da013b0c97479c51a423b1331f3184941c54936
danigutsch <daniutsch@outlook.com>
2024-12-22 18:08:14 +0100

    Add Price value object and Currency enumeration
    Introduces a new `Price` class in the
    `DigitalRightsManagement.Domain.ProductAggregate` namespace.
    This class encapsulates a monetary value and its currency,
    providing validation through a static `Create` method.
    Also defines a `Currency` enumeration with various currency types.

---
+++ src/DigitalRightsManagement.Domain/ProductAggregate/Price.cs
@@ -0,0 +1,46 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Common;
+
+namespace DigitalRightsManagement.Domain.ProductAggregate;
+
+public sealed class Price : ValueObject
+{
+    public decimal Value { get; }
+    public Currency Currency { get; }
+
+    private Price(decimal value, Currency currency)
+    {
+        Value = value;
+        Currency = currency;
+    }
+
+    public static Result<Price> Create(decimal value, Currency currency)
+    {
+        if (value < 0)
+        {
+            return Errors.Product.Create.InvalidPrice(value);
+        }
+
+        if (!Enum.IsDefined(currency))
+        {
+            return Errors.Product.Create.InvalidCurrency(currency);
+        }
+
+        return new Price(value, currency);
+    }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Value;
+        yield return Currency;
+    }
+}
+
+public enum Currency
+{
+    Euro,
+    Dollar,
+    BritishPound,
+    Yen,
+    BrazilianReal,
+}
