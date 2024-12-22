037ac4609c2afd412996111c5289991a6858f1e2
danigutsch <daniutsch@outlook.com>
2024-12-22 19:45:11 +0100

    Add domain event management to AggregateRoot
    Introduced a private readonly list for DomainEvent objects
    and added a method to facilitate adding events to the list.

--- src/DigitalRightsManagement.Common/AggregateRoot.cs
+++ src/DigitalRightsManagement.Common/AggregateRoot.cs
@@ -5,4 +5,6 @@ public class AggregateRoot : Entity
     protected AggregateRoot(Guid id) : base(id) { }
 
     private readonly List<DomainEvent> _domainEvents = [];
+
+    public void AddDomainEvent(DomainEvent domainEvent) => _domainEvents.Add(domainEvent);
 }
