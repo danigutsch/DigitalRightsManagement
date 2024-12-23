46b2d89baa9a91173889b0c58032fc74fd5cbb9f
danigutsch <daniutsch@outlook.com>
2024-12-23 14:25:04 +0100

    Add DescriptionUpdated record for product updates
    Introduced a new sealed record `DescriptionUpdated` in the
    `DigitalRightsManagement.Domain.ProductAggregate` namespace.
    This record captures the `ProductId`, `NewDescription`, and
    `OldDescription`, and inherits from `DomainEvent`. Also added
    a using directive for the `DigitalRightsManagement.Common`
    namespace.

---
+++ src/DigitalRightsManagement.Domain/ProductAggregate/DescriptionUpdated.cs
@@ -0,0 +1,5 @@
+﻿using DigitalRightsManagement.Common;
+
+namespace DigitalRightsManagement.Domain.ProductAggregate;
+
+public sealed record DescriptionUpdated(Guid ProductId, string NewDescription, string OldDescription) : DomainEvent;
