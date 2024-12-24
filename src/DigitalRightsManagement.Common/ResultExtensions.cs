80d30371351c6dff488cc739b98ae2455c5a1d4d
danigutsch <daniutsch@outlook.com>
2024-12-25 00:08:33 +0100

    Add ResultExtensions with DoubleBind methods
    Introduce a new static class `ResultExtensions` in the
    `DigitalRightsManagement.Common` namespace. This class
    includes two `DoubleBind` extension methods for the
    `Result` type: one for synchronous binding and another
    for asynchronous binding. These methods enhance result
    handling by allowing chaining of operations while
    preserving access to the original result value.

---
+++ src/DigitalRightsManagement.Common/ResultExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.Result;
+
+namespace DigitalRightsManagement.Common;
+
+public static class ResultExtensions
+{
+    /// <summary>
+    /// Binds a function to the result and returns a new result containing a tuple of the previous and next values.
+    /// </summary>
+    /// <typeparam name="TPrevious">The type of the previous result value.</typeparam>
+    /// <typeparam name="TNext">The type of the next result value.</typeparam>
+    /// <param name="previousResult">The previous result.</param>
+    /// <param name="bindFunc">The function to bind to the result.</param>
+    /// <returns>A new result containing a tuple of the previous and next values.</returns>
+    public static Result<(TPrevious, TNext)> DoubleBind<TPrevious, TNext>(this Result<TPrevious> previousResult, Func<TPrevious, Result<TNext>> bindFunc)
+    {
+        return previousResult.Bind(bindFunc).Map(next => (previousResult.Value, next));
+    }
+
+    /// <summary>
+    /// Asynchronously binds a function to the result and returns a new result containing a tuple of the previous and next values.
+    /// </summary>
+    /// <typeparam name="TPrevious">The type of the previous result value.</typeparam>
+    /// <typeparam name="TNext">The type of the next result value.</typeparam>
+    /// <param name="previousResultTask">The task representing the previous result.</param>
+    /// <param name="bindFunc">The function to bind to the result.</param>
+    /// <returns>A task representing the new result containing a tuple of the previous and next values.</returns>
+    public static Task<Result<(TPrevious Prev, TNext Next)>> DoubleBind<TPrevious, TNext>(this Task<Result<TPrevious>> previousResultTask, Func<TPrevious, Task<Result<TNext>>> bindFunc)
+    {
+        return previousResultTask.BindAsync(bindFunc).MapAsync(async next => ((await previousResultTask).Value, next));
+    }
+}
