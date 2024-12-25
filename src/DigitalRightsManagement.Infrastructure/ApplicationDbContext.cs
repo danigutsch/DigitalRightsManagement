6c73d7065c9637b033c19b23c65caabcfff80afc
danigutsch <daniutsch@outlook.com>
2024-12-25 01:07:51 +0100

    Add ApplicationDbContext and UserRepository classes
    Introduced the ApplicationDbContext class to manage database
    operations, implementing IUnitOfWork and including DbSet
    properties for User and Product. Added UserRepository class
    with a method to retrieve a User by its Guid identifier,
    returning a Result<User> to indicate the user's existence.

---
+++ src/DigitalRightsManagement.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DigitalRightsManagement.Application;
+using DigitalRightsManagement.Domain.ProductAggregate;
+using DigitalRightsManagement.Domain.UserAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalRightsManagement.Infrastructure;
+
+internal class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IUnitOfWork
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    public async Task SaveChanges(CancellationToken cancellationToken) => await SaveChangesAsync(cancellationToken);
+}
---
+++ src/DigitalRightsManagement.Infrastructure/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Domain.UserAggregate;
+
+namespace DigitalRightsManagement.Infrastructure;
+
+internal class UserRepository(ApplicationDbContext context)
+{
+    public async Task<Result<User>> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var user = await context.Users.FindAsync([id], cancellationToken);
+        return user ?? Result<User>.NotFound();
+    }
+}
