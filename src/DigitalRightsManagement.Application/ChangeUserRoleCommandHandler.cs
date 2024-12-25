a4296a3cc77e76e6c10c5567da5fa76ea1e015a7
danigutsch <daniutsch@outlook.com>
2024-12-25 12:52:42 +0100

    Refactor user role change logic into command handler
    Added `ChangeUserRoleCommandHandler` to encapsulate user role change functionality using a command pattern. This replaces the previous implementation in `UserService`, promoting better separation of concerns and improving testability. The `UserService` class has been removed to reflect this design shift.

---
+++ src/DigitalRightsManagement.Application/ChangeUserRoleCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Common;
+using DigitalRightsManagement.Domain.UserAggregate;
+
+namespace DigitalRightsManagement.Application;
+
+public sealed class ChangeUserRoleCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+{
+    public async Task<Result> Handle(ChangeUserRoleCommand command, CancellationToken ct)
+    {
+        return await userRepository.GetById(command.AdminId, ct)
+            .DoubleBind(_ => userRepository.GetById(command.TargetId, ct))
+            .BindAsync(t => t.Prev.ChangeRole(t.Next, command.DesiredRole))
+            .Tap(_ => unitOfWork.SaveChanges(ct));
+    }
+}
+
+public sealed record ChangeUserRoleCommand(Guid AdminId, Guid TargetId, UserRoles DesiredRole);
