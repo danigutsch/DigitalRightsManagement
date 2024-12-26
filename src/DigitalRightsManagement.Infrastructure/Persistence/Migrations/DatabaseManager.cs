f53878b3bde93e0c4a5bea03367243d4253d9c9a
danigutsch <daniutsch@outlook.com>
2024-12-26 02:13:46 +0100

    Add DatabaseManager for database initialization and management
    Introduces the `DatabaseManager` class implementing the `IDatabaseManager` interface. This class ensures the database exists and initializes it with user data using Entity Framework Core's execution strategy and transaction management. The `EnsureDatabase` method checks for database existence and creates it if necessary, while the `InitializeDatabase` method applies migrations and adds users within a transaction. Relevant namespaces are also imported.

---
+++ src/DigitalRightsManagement.Infrastructure/Persistence/Migrations/DatabaseManager.cs
@@ -0,0 +1,46 @@
+﻿using DigitalRightsManagement.Domain.UserAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DigitalRightsManagement.Infrastructure.Persistence.Migrations;
+
+internal sealed class DatabaseManager(ApplicationDbContext context) : IDatabaseManager
+{
+    public async Task EnsureDatabase(CancellationToken ct)
+    {
+        var dbCreator = context.GetService<IRelationalDatabaseCreator>();
+
+        var strategy = context.Database.CreateExecutionStrategy();
+        await strategy.ExecuteAsync(async () =>
+        {
+            if (!await dbCreator.ExistsAsync(ct))
+            {
+                await dbCreator.CreateAsync(ct);
+            }
+        });
+    }
+
+    public async Task InitializeDatabase(IEnumerable<User> users, CancellationToken ct)
+    {
+        var strategy = context.Database.CreateExecutionStrategy();
+        await strategy.ExecuteAsync(async () =>
+        {
+            await using var transaction = await context.Database.BeginTransactionAsync(ct);
+
+            await context.Database.MigrateAsync(ct);
+
+            var users = await context.Users.ToArrayAsync();
+
+            context.Users.AddRange(users);
+
+            await transaction.CommitAsync(ct);
+        });
+    }
+}
+
+public interface IDatabaseManager
+{
+    Task EnsureDatabase(CancellationToken ct);
+    Task InitializeDatabase(IEnumerable<User> users, CancellationToken ct);
+}
