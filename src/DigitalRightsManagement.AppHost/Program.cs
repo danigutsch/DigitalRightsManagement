0fa1581406314b33d0274b6482ae4872f5fa83bf
danigutsch <daniutsch@outlook.com>
2024-12-26 19:45:44 +0100

    Add migration service reference and update initialization
    Updated `DigitalRightsManagement.AppHost.csproj` to include
    a reference to `DigitalRightsManagement.MigrationService.csproj`.
    Modified `Program.cs` to initialize the migration service and
    ensure proper dependencies are established for the
    `DigitalRightsManagement_Api` project, enhancing the
    application's startup sequence.

--- src/DigitalRightsManagement.AppHost/Program.cs
+++ src/DigitalRightsManagement.AppHost/Program.cs
@@ -3,8 +3,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 var database = builder.AddPostgres("database")
     .WithPgWeb();
 
-builder.AddProject<Projects.DigitalRightsManagement_Api>("digitalrightsmanagement-api")
+var dbInitializer = builder.AddProject<Projects.DigitalRightsManagement_MigrationService>("digitalrightsmanagement-migrationservice")
     .WithReference(database)
     .WaitFor(database);
+
+builder.AddProject<Projects.DigitalRightsManagement_Api>("digitalrightsmanagement-api")
+    .WithReference(database)
+    .WaitFor(database)
+    .WithReference(dbInitializer)
+    .WaitForCompletion(dbInitializer);
 
 await builder.Build().RunAsync();
