67ee9bb12ac4b0546435e3d308847dd40c07fcef
danigutsch <daniutsch@outlook.com>
2024-12-26 19:46:22 +0100

    Update DependencyInjection for database connection handling
    - Added using directive for migrations.
    - Changed database connection string to a hardcoded value "database" in `AddInfrastructure`.
    - Introduced `AddMigrationInfrastructure` method to set up the database context and register `IDatabaseManager`.

--- src/DigitalRightsManagement.Infrastructure/DependencyInjection.cs
+++ src/DigitalRightsManagement.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using DigitalRightsManagement.Application;
 using DigitalRightsManagement.Infrastructure.Persistence;
+using DigitalRightsManagement.Infrastructure.Persistence.Migrations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,11 +10,20 @@ public static class DependencyInjection
 {
     public static THostBuilder AddInfrastructure<THostBuilder>(this THostBuilder builder) where THostBuilder : IHostApplicationBuilder
     {
-        builder.AddNpgsqlDbContext<ApplicationDbContext>(PersistenceDefaults.ConnectionStringName);
+        builder.AddNpgsqlDbContext<ApplicationDbContext>("database");
 
         builder.Services
             .AddScoped<IUnitOfWork, ApplicationDbContext>()
             .AddScoped<IUserRepository, UserRepository>();
+
+        return builder;
+    }
+
+    public static THostBuilder AddMigrationInfrastructure<THostBuilder>(this THostBuilder builder) where THostBuilder : IHostApplicationBuilder
+    {
+        builder.AddNpgsqlDbContext<ApplicationDbContext>("database");
+
+        builder.Services.AddScoped<IDatabaseManager, DatabaseManager>();
 
         return builder;
     }
