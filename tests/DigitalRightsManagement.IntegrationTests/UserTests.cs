ae397dd7ce08819332809a54fa1dbd3f316ca073
danigutsch <daniutsch@outlook.com>
2024-12-28 01:17:47 +0100

    Add IntegrationTestsBase and refactor UserTests
    Introduced the `IntegrationTestsBase` class to manage the lifecycle of integration tests and provide a shared `HttpClient`. Updated `UserTests` to inherit from this base class, simplifying the test setup and renaming the `Change_Role_Returns_No_Content` method to `Change_Role_Returns_Success`. The implementation now consistently uses the `HttpClient` from the base class.

--- tests/DigitalRightsManagement.IntegrationTests/UserTests.cs
+++ tests/DigitalRightsManagement.IntegrationTests/UserTests.cs
@@ -1,40 +1,27 @@
 ﻿using DigitalRightsManagement.Api;
-using DigitalRightsManagement.AppHost;
 using DigitalRightsManagement.Domain.UserAggregate;
 using DigitalRightsManagement.MigrationService;
 using FluentAssertions;
 using System.Net.Http.Json;
+using Xunit.Abstractions;
 
 namespace DigitalRightsManagement.IntegrationTests;
 
-public sealed class UserTests
+public sealed class UserTests(ITestOutputHelper outputHelper) : IntegrationTestsBase(outputHelper)
 {
     [Fact]
-    public async Task Change_Role_Returns_No_Content()
+    public async Task Change_Role_Returns_Success()
     {
         // Arrange
-        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.DigitalRightsManagement_AppHost>();
-        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-        {
-            clientBuilder.AddStandardResilienceHandler();
-        });
-
-        await using var app = await appHost.BuildAsync();
-        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-
-        await app.StartAsync();
-
-        // Act
-        using var httpClient = app.CreateHttpClient(ResourceNames.Api);
-        await resourceNotificationService.WaitForResourceAsync(ResourceNames.Api, KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-
         var admin = SeedData.Users.First(u => u.Role == UserRoles.Admin);
         var target = SeedData.Users.First(u => u.Role == UserRoles.Viewer);
         const UserRoles desiredRole = UserRoles.Admin;
 
         var changeRoleDto = new ChangeUserDto(admin.Id, target.Id, desiredRole);
+
+        // Act
         using var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
-        var response = await httpClient.PostAsJsonAsync("/users/change-role", changeRoleDto, cts.Token);
+        var response = await HttpClient.PostAsJsonAsync("/users/change-role", changeRoleDto, cts.Token);
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
