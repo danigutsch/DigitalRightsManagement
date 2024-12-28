9d6e8f14ce179e4093bae947121a3e4f7d7843db
danigutsch <daniutsch@outlook.com>
2024-12-28 22:30:56 +0100

    Add integration tests for product creation
    This commit introduces a new test class `ProductTests` in the `ProductTests.cs` file. It includes a test method `Add_Product_Returns_Success` that verifies the successful creation of a product using randomly generated data. The test sends a POST request to the `/products/create` endpoint and asserts that the response status code is `OK` (200).

---
+++ tests/DigitalRightsManagement.IntegrationTests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using DigitalRightsManagement.Api.Endpoints;
+using DigitalRightsManagement.Domain.ProductAggregate;
+using DigitalRightsManagement.Domain.UserAggregate;
+using DigitalRightsManagement.MigrationService;
+using FluentAssertions;
+using System.Net.Http.Json;
+using Xunit.Abstractions;
+
+namespace DigitalRightsManagement.IntegrationTests;
+
+public sealed class ProductTests(ITestOutputHelper outputHelper) : IntegrationTestsBase(outputHelper)
+{
+    [Fact]
+    public async Task Add_Product_Returns_Success()
+    {
+        // Arrange
+        var productName = Faker.Commerce.ProductName();
+        var productDescription = Faker.Commerce.ProductDescription();
+        var productPrice = Faker.Random.Decimal(1, 100);
+        var productCurrency = Faker.PickRandom<Currency>();
+
+        var manager = SeedData.Users.First(u => u.Role == UserRoles.Manager);
+
+        var createProductDto = new CreateProductDto(manager.Id, productName, productDescription, productPrice, productCurrency);
+
+        // Act
+        var response = await HttpClient.PostAsJsonAsync("/products/create", createProductDto);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
