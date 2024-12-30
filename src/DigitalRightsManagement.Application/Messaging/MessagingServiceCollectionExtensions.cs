beb48ff54f32baba2a5e92f200935a5d62ca03a3
danigutsch <daniutsch@outlook.com>
2024-12-30 23:30:21 +0100

    Refactor dependency injection for messaging services
    Removed specific command handler registrations in favor of a new `AddMessaging()` method in `DependencyInjection.cs`. Introduced `MessagingServiceCollectionExtensions` to register MediatR services, enhancing the application's messaging capabilities.

---
+++ src/DigitalRightsManagement.Application/Messaging/MessagingServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DigitalRightsManagement.Application.Messaging;
+
+internal static class MessagingServiceCollectionExtensions
+{
+    public static IServiceCollection AddMessaging(this IServiceCollection services) =>
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblyContaining<IUnitOfWork>());
+}
