3cf86df31845045af6a837b3fadf49bad5618d8c
danigutsch <daniutsch@outlook.com>
2025-01-11 13:27:39 +0100

    Rename BasicAuthUser to AuthUser
    Updated the class name from `BasicAuthUser` to `AuthUser`.

--- src/DigitalRightsManagement.Infrastructure/Persistence/Identity/AuthUser.cs
+++ src/DigitalRightsManagement.Infrastructure/Persistence/Identity/AuthUser.cs
@@ -2,7 +2,7 @@
 
 namespace DigitalRightsManagement.Infrastructure.Persistence.Identity;
 
-public sealed class BasicAuthUser : IdentityUser
+public sealed class AuthUser : IdentityUser
 {
     public required Guid DomainUserId { get; init; }
 }
