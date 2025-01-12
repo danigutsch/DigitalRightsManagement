0002cf7e65434bfda84c427ae0591c016ddf100b
danigutsch <daniutsch@outlook.com>
2025-01-12 14:27:45 +0100

    Add ICurrentUserProvider interface for user retrieval
    This commit introduces the `ICurrentUserProvider` interface in the `DigitalRightsManagement.Infrastructure.Identity` namespace. It includes a `Get` method that returns a `Task<Result<User>>`, allowing for asynchronous retrieval of the current user with a `CancellationToken`. Necessary using directives for `Ardalis.Result` and `DigitalRightsManagement.Domain.UserAggregate` are also added.

---
+++ src/DigitalRightsManagement.Application/ICurrentUserProvider.cs
@@ -0,0 +1,9 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Domain.UserAggregate;
+
+namespace DigitalRightsManagement.Infrastructure.Identity;
+
+public interface ICurrentUserProvider
+{
+    Task<Result<User>> Get(CancellationToken ct);
+}
