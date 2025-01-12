9eeefbae5a2a92881f29647879d3e14e1751ff28
danigutsch <daniutsch@outlook.com>
2025-01-12 12:01:25 +0100

    Add basic authentication mechanism
    Introduce classes and methods for basic authentication in the Digital Rights Management system. This includes `AuthenticationDependencyInjection` for service configuration, `BasicAuthenticationDefaults` for defining the authentication scheme, and `BasicAuthenticationHandler` for processing authentication requests. Additionally, implement `BasicAuthenticationHeaderValue` to parse and validate the authorization header format, enabling user authentication with username and password credentials.

---
+++ src/DigitalRightsManagement.Infrastructure/Authentication/AuthenticationDependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace DigitalRightsManagement.Infrastructure.Authentication;
+
+public static class AuthenticationDependencyInjection
+{
+    public static AuthenticationBuilder AddBasic(this AuthenticationBuilder builder)
+    {
+        return builder.AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>(BasicAuthenticationDefaults.AuthenticationScheme, null);
+    }
+}
---
+++ src/DigitalRightsManagement.Infrastructure/Authentication/BasicAuthenticationDefaults.cs
@@ -0,0 +1,6 @@
+﻿namespace DigitalRightsManagement.Infrastructure.Authentication;
+
+public static class BasicAuthenticationDefaults
+{
+    public const string AuthenticationScheme = "Basic";
+}
