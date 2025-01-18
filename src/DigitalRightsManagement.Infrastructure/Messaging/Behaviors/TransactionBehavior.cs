79d84f81a9aa1c3a7c7bffe8817bba893c614824
danigutsch <daniutsch@outlook.com>
2025-01-18 21:31:20 +0100

    Refactor service registration and messaging structure
    - Removed `AddApplication`, `AddInfrastructure`, and `AddIdentityInfrastructure` from `Program.cs`, adding `AddServiceDefaults` instead.
    - Deleted the `AddApplication` method from `ApplicationDependencyInjection.cs`, indicating a shift in messaging handling.
    - Introduced `AddMessaging` in `InfrastructureDependencyInjection.cs` to integrate messaging capabilities into the infrastructure layer.
    - Added `TransactionBehavior.cs` to manage database transactions for MediatR requests, enhancing transaction handling and logging.
    - Updated namespace in `MessagingServiceCollectionExtensions.cs` from `DigitalRightsManagement.Application.Messaging` to `DigitalRightsManagement.Infrastructure.Messaging` to reflect the reorganization of messaging code.

---
+++ src/DigitalRightsManagement.Infrastructure/Messaging/Behaviors/TransactionBehavior.cs
@@ -0,0 +1,58 @@
+﻿using DigitalRightsManagement.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace DigitalRightsManagement.Infrastructure.Messaging.Behaviors;
+
+internal sealed class TransactionBehavior<TRequest, TResponse>(
+    ApplicationDbContext dbContext,
+    ILogger<TransactionBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var typeName = typeof(TRequest).Name;
+
+        try
+        {
+            if (dbContext.Database.CurrentTransaction is not null)
+            {
+                return await next();
+            }
+
+            await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+            using (logger.BeginScope(new List<KeyValuePair<string, object>> { new("TransactionContext", transaction.TransactionId) }))
+            {
+                logger.BeginTransaction(transaction.TransactionId, typeName, request);
+
+                var response = await next();
+
+                logger.CommitTransaction(transaction.TransactionId, typeName);
+
+                await transaction.CommitAsync(cancellationToken);
+
+                return response;
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.TransactionError(ex, typeName, request);
+
+            throw;
+        }
+    }
+}
+
+internal static partial class TransactionBehaviorLogger
+{
+    [LoggerMessage(LogLevel.Information, "Begin transaction {TransactionId} for {CommandName} ({@Command})")]
+    public static partial void BeginTransaction(this ILogger logger, Guid transactionId, string commandName, object command);
+
+    [LoggerMessage(LogLevel.Information, "Commit transaction {TransactionId} for {CommandName}")]
+    public static partial void CommitTransaction(this ILogger logger, Guid transactionId, string commandName);
+
+    [LoggerMessage(LogLevel.Error, "Error Handling transaction for {CommandName} ({@Command})")]
+    public static partial void TransactionError(this ILogger logger, Exception exception, string commandName, object command);
+}
