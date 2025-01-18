7cdad656b2d2db135d3ff3cd61d7cd080d6a1436
danigutsch <daniutsch@outlook.com>
2025-01-18 21:39:32 +0100

    Refactor dependency injection for persistence services
    This commit refactors the dependency injection setup in the Digital Rights Management application. The direct registrations of the database context and repositories in `InfrastructureDependencyInjection.cs` have been replaced with a call to a new method `AddPersistence()` in `PersistenceDependencyInjection.cs`. This change improves code organization and separation of concerns. Additionally, new using directives were added to support the updated functionality.

---
+++ src/DigitalRightsManagement.Infrastructure/Persistence/PersistenceDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using DigitalRightsManagement.Application.Persistence;
+using DigitalRightsManagement.Common;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DigitalRightsManagement.Infrastructure.Persistence;
+
+internal static class PersistenceDependencyInjection
+{
+    public static THostBuilder AddPersistence<THostBuilder>(this THostBuilder builder) where THostBuilder : IHostApplicationBuilder
+    {
+        builder.AddNpgsqlDbContext<ApplicationDbContext>(ResourceNames.Database, settings => settings.DisableRetry = true);
+
+        builder.Services
+            .AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<ApplicationDbContext>())
+            .AddScoped<IUserRepository, UserRepository>()
+            .AddScoped<IProductRepository, ProductRepository>();
+
+        return builder;
+    }
+}
