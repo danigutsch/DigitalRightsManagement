d4e74e7e977e2d853f1d97b69cdeb7c86803a19e
danigutsch <daniutsch@outlook.com>
2025-01-18 20:36:31 +0100

    Enhance ApplicationDbContext to publish domain events
    Updated ApplicationDbContext to include a publisher parameter in the constructor. Modified the SaveChanges method to publish domain events before saving changes asynchronously. Added necessary using directives for new functionality.

--- src/DigitalRightsManagement.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/DigitalRightsManagement.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,21 +1,29 @@
 ﻿using DigitalRightsManagement.Application.Persistence;
 using DigitalRightsManagement.Domain.ProductAggregate;
 using DigitalRightsManagement.Domain.UserAggregate;
+using DigitalRightsManagement.Infrastructure.Messaging;
 using DigitalRightsManagement.Infrastructure.Persistence.EntityTypeConfigurations;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace DigitalRightsManagement.Infrastructure.Persistence;
 
-internal sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IUnitOfWork
+internal sealed class ApplicationDbContext(IPublisher publisher, DbContextOptions<ApplicationDbContext> options) : DbContext(options), IUnitOfWork
 {
     public DbSet<User> Users => Set<User>();
     public DbSet<Product> Products => Set<Product>();
-
-    public async Task SaveChanges(CancellationToken cancellationToken) => await SaveChangesAsync(cancellationToken);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new UserConfiguration());
         modelBuilder.ApplyConfiguration(new ProductConfiguration());
+    }
+
+    public async Task SaveChanges(CancellationToken cancellationToken)
+    {
+        await publisher.PublishDomainEvents(this, cancellationToken);
+
+        await SaveChangesAsync(cancellationToken)
+            .ConfigureAwait(false);
     }
 }
