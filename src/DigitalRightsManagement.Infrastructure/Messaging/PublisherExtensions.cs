545a93f99b50b0cb8ab02cf96d84ae2a494d6803
danigutsch <daniutsch@outlook.com>
2025-01-18 20:33:46 +0100

    Add PublisherExtensions for domain event publishing
    This commit introduces a new static class `PublisherExtensions` in the `DigitalRightsManagement.Infrastructure.Messaging` namespace. It includes an extension method `PublishDomainEvents` for the `IPublisher` interface, which retrieves and publishes domain events from the `ChangeTracker` of a `DbContext` for `AggregateRoot` entities asynchronously.

---
+++ src/DigitalRightsManagement.Infrastructure/Messaging/PublisherExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DigitalRightsManagement.Common.DDD;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalRightsManagement.Infrastructure.Messaging;
+
+internal static class PublisherExtensions
+{
+    public static async Task PublishDomainEvents(this IPublisher publisher, DbContext dbContext, CancellationToken cancellationToken)
+    {
+        var domainEvents = dbContext.ChangeTracker.Entries<AggregateRoot>()
+            .SelectMany(e => e.Entity.PopDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await publisher.Publish(domainEvent, cancellationToken);
+        }
+    }
+}
