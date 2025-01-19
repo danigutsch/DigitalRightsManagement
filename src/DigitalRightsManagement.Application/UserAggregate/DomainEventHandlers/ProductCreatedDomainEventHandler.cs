750496ed78760f48b9c3cb246d4bcbd4f6c4c7a6
danigutsch <daniutsch@outlook.com>
2025-01-19 19:35:02 +0100

    Add ProductCreatedDomainEventHandler for user product management
    Introduces the `ProductCreatedDomainEventHandler` class to handle the `ProductCreated` domain event. This handler retrieves the user by ID and adds the newly created product to their list, ensuring changes are saved to the user repository's unit of work. Additionally, updates in `CreateProduct.cs` ensure that the product is correctly associated with the user ID during creation.

---
+++ src/DigitalRightsManagement.Application/UserAggregate/DomainEventHandlers/ProductCreatedDomainEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using DigitalRightsManagement.Application.Messaging;
+using DigitalRightsManagement.Application.Persistence;
+using DigitalRightsManagement.Common.DDD;
+using DigitalRightsManagement.Domain.ProductAggregate.Events;
+
+namespace DigitalRightsManagement.Application.UserAggregate.DomainEventHandlers;
+
+public sealed class ProductCreatedDomainEventHandler(IUserRepository userRepository) : IDomainEventHandler<ProductCreated>
+{
+    public async Task Handle(ProductCreated notification, CancellationToken cancellationToken)
+    {
+        await userRepository.GetById(notification.UserId, cancellationToken)
+            .Tap(t => t.AddProduct(notification.ProductId))
+            .Tap(_ => userRepository.UnitOfWork.SaveEntities(cancellationToken));
+    }
+}
