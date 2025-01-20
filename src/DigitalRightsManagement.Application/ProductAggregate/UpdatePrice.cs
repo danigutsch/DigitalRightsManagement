f5cb0c53c83c196ff18dfb156fdd109dbe83b499
danigutsch <daniutsch@outlook.com>
2025-01-20 18:56:55 +0100

    Add product price update functionality and tests
    - Implemented a new endpoint in `ProductEndpoints` for updating product prices, including necessary metadata and authorization.
    - Modified product entries in `SeedData.cs` to include a new `Guid` parameter.
    - Added a test case in `ProductTests` to verify the price update feature for managers.
    - Introduced `UpdatePrice.cs` with the command and handler for processing price updates, including user validation and persistence.

---
+++ src/DigitalRightsManagement.Application/ProductAggregate/UpdatePrice.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Application.Messaging;
+using DigitalRightsManagement.Application.Persistence;
+using DigitalRightsManagement.Common.DDD;
+using DigitalRightsManagement.Common.Messaging;
+using DigitalRightsManagement.Domain.ProductAggregate;
+
+namespace DigitalRightsManagement.Application.ProductAggregate;
+
+public sealed class UpdatePrice(ICurrentUserProvider currentUserProvider, IProductRepository productRepository) : ICommandHandler<UpdatePriceCommand>
+{
+    public async Task<Result> Handle(UpdatePriceCommand command, CancellationToken cancellationToken)
+    {
+        var userResult = await currentUserProvider.Get(cancellationToken);
+        if (!userResult.IsSuccess)
+        {
+            return userResult.Map();
+        }
+
+        var user = userResult.Value;
+
+        var productResult = await productRepository.GetById(command.ProductId, cancellationToken);
+        if (!productResult.IsSuccess)
+        {
+            return productResult.Map();
+        }
+
+        var product = productResult.Value;
+
+        return await Price.Create(command.NewPrice, command.Currency)
+            .Tap(price => product.UpdatePrice(user.Id, price, command.Reason))
+            .Tap(_ => productRepository.UnitOfWork.SaveEntities(cancellationToken))
+            .MapAsync(_ => Result.Success());
+    }
+}
+
+public sealed record UpdatePriceCommand(Guid ProductId, decimal NewPrice, Currency Currency, string Reason) : ICommand;
