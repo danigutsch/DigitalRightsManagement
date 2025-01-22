3b880d65a733625642d4d62b3dbbdcb9b97d702c
danigutsch <daniutsch@outlook.com>
2025-01-22 22:15:58 +0100

    Rename AddPolicies to AddDefaultPolicies for clarity
    Updated the authentication and authorization setup in the .NET application.
    Renamed the method for adding authorization policies in `Program.cs` and
    modified its definition in `Policies.cs` to maintain consistency.
    This change standardizes the naming convention for default authorization policies.

--- src/DigitalRightsManagement.Infrastructure/Identity/Policies.cs
+++ src/DigitalRightsManagement.Infrastructure/Identity/Policies.cs
@@ -7,7 +7,7 @@ public static class Policies
     public const string IsAdmin = "IsAdmin";
     public const string IsManager = "IsManager";
 
-    public static AuthorizationBuilder AddPolicies(this AuthorizationBuilder builder)
+    public static AuthorizationBuilder AddDefaultPolicies(this AuthorizationBuilder builder)
     {
         builder.AddPolicy(IsAdmin, policy => policy.RequireRole(AuthRoles.Admin));
         builder.AddPolicy(IsManager, policy => policy.RequireRole(AuthRoles.Manager));
