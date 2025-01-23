0d572d57a9af61bee2f704c4dc0bfa27057872de
danigutsch <daniutsch@outlook.com>
2025-01-23 19:40:04 +0100

    Rename "Manager" column to "UserId" in Products table
    This commit updates the database schema by renaming the "Manager" column to "UserId" in the "Products" table. The migration class `ChangeNameOfProductOwnerColumn` has been modified to include the necessary changes in both the `Up` and `Down` methods. Additionally, the model snapshot has been updated to reflect this change, replacing the "Manager" property with "UserId" in the `Product` entity definition.

--- src/DigitalRightsManagement.Infrastructure/Persistence/Migrations/20250123183901_ChangeNameOfProductOwnerColumn.Designer.cs
+++ src/DigitalRightsManagement.Infrastructure/Persistence/Migrations/20250123183901_ChangeNameOfProductOwnerColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DigitalRightsManagement.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DigitalRightsManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250123183901_ChangeNameOfProductOwnerColumn")]
+    partial class ChangeNameOfProductOwnerColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
