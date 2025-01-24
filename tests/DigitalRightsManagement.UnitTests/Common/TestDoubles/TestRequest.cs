5d956a4a62c8d01d641099390b88faa01a42e51a
danigutsch <daniutsch@outlook.com>
2025-01-24 18:25:53 +0100

    Refactor authorization requests and tests
    Introduce a new `TestRequest` class in `TestRequest.cs` as a base for authorization requests. Add `AuthorizedRequestWithoutRole` and `AuthorizedRequest` classes, with the latter specifically for users with the `Manager` role. Remove local definitions of these classes from `AuthorizationBehaviorTests.cs` and eliminate the import for `DigitalRightsManagement.Application.Authorization`. Core test functionality remains unchanged.

---
+++ tests/DigitalRightsManagement.UnitTests/Common/TestDoubles/TestRequest.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Application.Authorization;
+using DigitalRightsManagement.Domain.UserAggregate;
+using MediatR;
+
+namespace DigitalRightsManagement.UnitTests.Common.TestDoubles;
+
+internal class TestRequest : IRequest<Result> { }
+
+[Authorize]
+internal sealed class AuthorizedRequestWithoutRole : TestRequest;
+
+[Authorize(UserRoles.Manager)]
+internal sealed class AuthorizedRequest : TestRequest;
