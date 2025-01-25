10b6dcb2af8be96152d1129eeadc883c8e44ca0c
danigutsch <daniutsch@outlook.com>
2025-01-25 09:21:12 +0100

    Add authorization attributes to request classes
    Updated `AuthorizedRequest` and `AuthorizedRequestWithoutRole` to include authorization attributes and relevant namespaces. Removed unnecessary namespaces and attributes from `TestRequest`.

---
+++ tests/DigitalRightsManagement.UnitTests/Common/TestDoubles/AuthorizedRequestWithoutRole.cs
@@ -0,0 +1,6 @@
+﻿using DigitalRightsManagement.Application.Authorization;
+
+namespace DigitalRightsManagement.UnitTests.Common.TestDoubles;
+
+[Authorize]
+internal sealed class AuthorizedRequestWithoutRole : TestRequest;
