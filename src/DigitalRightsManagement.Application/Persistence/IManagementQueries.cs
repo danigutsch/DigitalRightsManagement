0bd2f4de0a4e1370425efb28b50bd60a26a08992
danigutsch <daniutsch@outlook.com>
2025-01-26 01:15:59 +0100

    Add IManagementQueries interface and ManagementQueries class
    Introduces the `IManagementQueries` interface with a `GetById` method for retrieving users by their ID. Implements the `ManagementQueries` class, which uses Entity Framework to fetch users asynchronously and returns a `Result<User>` indicating if the user was found.

---
+++ src/DigitalRightsManagement.Application/Persistence/IManagementQueries.cs
@@ -0,0 +1,9 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Domain.UserAggregate;
+
+namespace DigitalRightsManagement.Application.Persistence;
+
+public interface IManagementQueries
+{
+    Task<Result<User>> GetById(Guid id, CancellationToken cancellationToken);
+}
---
+++ src/DigitalRightsManagement.Infrastructure/Persistence/ManagementQueries.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Application.Persistence;
+using DigitalRightsManagement.Domain.UserAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalRightsManagement.Infrastructure.Persistence;
+
+internal sealed class ManagementQueries(ManagementDbContext dbContext) : IManagementQueries
+{
+    public async Task<Result<User>> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var user = await dbContext.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(user => user.Id == id, cancellationToken: cancellationToken);
+
+        return user ?? Result<User>.NotFound();
+    }
+}
