46f38b8cce3d9f9db7e7ac660ca3970e39ff473e
danigutsch <daniutsch@outlook.com>
2025-01-26 11:31:36 +0100

    Remove ListOfGuidComparer and ListOfGuidConverter
    This commit removes the `ListOfGuidComparer` and `ListOfGuidConverter` classes, which were responsible for comparing and converting lists of GUIDs. The import statement for these converters was also removed from `UserConfiguration.cs`, reflecting their obsolescence. Additionally, the configuration for the `_products` property in `UserConfiguration` was updated to use the new `PrimitiveCollection` method, indicating a shift in how product collections are managed within the entity configuration.

--- src/DigitalRightsManagement.Infrastructure/Persistence/Converters/ListOfGuidComparer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
-
-namespace DigitalRightsManagement.Infrastructure.Persistence.Converters;
-
-internal sealed class ListOfGuidComparer()
-    : ValueComparer<List<Guid>>(
-        (e1, e2) => e1!.SequenceEqual(e2!),
-        e => e.Aggregate(0, HashCode.Combine),
-        e => e.Select(v => v).ToList());
