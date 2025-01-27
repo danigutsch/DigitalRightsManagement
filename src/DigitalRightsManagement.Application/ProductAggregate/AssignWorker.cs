4b4bd3e9e8715584c1f0b1464d7f08b889512d9a
danigutsch <daniutsch@outlook.com>
2025-01-27 18:46:07 +0100

    Add InvalidWorkerRole error and AssignWorker command
    Updated the `Errors` class with a new method `InvalidWorkerRole` for handling invalid worker role assignments. Introduced `AssignWorkerCommand` and its handler in `AssignWorker.cs` to manage the assignment of workers to products, including necessary validation and error handling.

---
+++ src/DigitalRightsManagement.Application/ProductAggregate/AssignWorker.cs
@@ -0,0 +1,49 @@
+﻿using Ardalis.Result;
+using DigitalRightsManagement.Application.Authorization;
+using DigitalRightsManagement.Application.Messaging;
+using DigitalRightsManagement.Application.Persistence;
+using DigitalRightsManagement.Common.DDD;
+using DigitalRightsManagement.Common.Messaging;
+using DigitalRightsManagement.Domain;
+using DigitalRightsManagement.Domain.AgentAggregate;
+using DigitalRightsManagement.Domain.ProductAggregate;
+
+namespace DigitalRightsManagement.Application.ProductAggregate;
+
+[AuthorizeResourceOwner<Product>]
+public sealed record AssignWorkerCommand(Guid ProductId, Guid WorkerId) : ICommand
+{
+    internal sealed class AssignWorkerCommandHandler(
+        ICurrentAgentProvider currentUserProvider,
+        IAgentRepository userRepository,
+        IProductRepository productRepository) : ICommandHandler<AssignWorkerCommand>
+    {
+        public async Task<Result> Handle(AssignWorkerCommand command, CancellationToken cancellationToken)
+        {
+            var currentAgentResult = await currentUserProvider.Get(cancellationToken);
+            if (!currentAgentResult.IsSuccess)
+            {
+                return currentAgentResult.Map();
+            }
+
+            var currentAgent = currentAgentResult.Value;
+
+            var workerResult = await userRepository.GetById(command.WorkerId, cancellationToken);
+            if (!workerResult.IsSuccess)
+            {
+                return workerResult.Map();
+            }
+
+            var worker = workerResult.Value;
+
+            if (worker.Role != AgentRoles.Worker)
+            {
+                return Errors.Agents.InvalidWorkerRole(worker.Id, worker.Role);
+            }
+
+            return await productRepository.GetById(command.ProductId, cancellationToken)
+                .BindAsync(product => product.AssignWorker(currentAgent.Id, worker.Id))
+                .Tap(() => productRepository.UnitOfWork.SaveEntities(cancellationToken));
+        }
+    }
+}
